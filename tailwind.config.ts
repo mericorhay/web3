8beab9b8e7ec05c150386abf4ff18c84e64cbffc
mericorhay <108902339+mericorhay@users.noreply.github.com>
2025-10-21 17:13:31 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    // Bu satırlar Vercel'in doğru dosyaları bulmasını sağlayacak:
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
